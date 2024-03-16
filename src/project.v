252a967819462bd57c61e8c2d180d1230dae8836
sree-75 <163626024+sree-75@users.noreply.github.com>
2024-03-16 15:26:12 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -18,9 +18,9 @@ module tt_um_HA (
 
   // All output pins must be assigned. If not used, assign to 0.
   // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-    assign uo_in[7:2]=6'b000000;
+   // assign uo_in[7:2]=6'b000000;
     assign uo_out[7:2]=6'b000000;
-    assign uio_in = 0;
+  //  assign uio_in = 0;
   assign uio_out = 0;
   assign uio_oe  = 0;
     assign uo_out[0] = ui_in[0] ^ui_in[1];// assigning sum
